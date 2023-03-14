53aa722fbfe1ec583dc855e2938ce96885a8c2c1
maximebarber <maximebarber@gmail.com>
2023-03-14 14:45:33 +0100

    Added test route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from './components/Login.vue'
 import Shtats from './components/Shtats.vue'
+import Testy from './components/Testy.vue'
 
 const router = createRouter({
   history: createWebHistory('/dust-shtats/'),
