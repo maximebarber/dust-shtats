15728fb020613d0650585f5878e7559bd60218ac
maximebarber <maximebarber@gmail.com>
2023-03-14 14:39:34 +0100

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/dust-shtats/'
+  base: '/dust-shtats/',
+  build: {
+    publicPath: 'https://maximebarber.github.io/dust-shtats'
+  }
 })
