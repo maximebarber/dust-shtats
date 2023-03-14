1f8ef708b9accd499200ff20b3736ac8e44b8696
maximebarber <maximebarber@gmail.com>
2023-03-14 13:27:29 +0100

    Added hg

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Shtats from './components/Shtats.vue'
-import Login from './components/Login.vue';
 </script>
 
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo_hg.png" />-->
+  <img alt="Vue logo" src="./assets/logo_hg.png" />
   <h1>Dust Shtats</h1>
   <router-view />
-  <!-- <Login />
-  <Shtats /> -->
 </template>
 
 <style>
