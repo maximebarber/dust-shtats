a4a09b7c470abae78fa3cbad5f247e6ff7023e25
maximebarber <maximebarber@gmail.com>
2023-03-14 14:19:17 +0100

    Changed redirect uri

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -20,7 +20,8 @@ export default {
   methods: {
     login() {
       const client_id = 'd5c90c3559e747e598fe18eccc6cd1a0'; // Your client id
-      const redirect_uri = 'http://localhost:4173';
+      //const redirect_uri = 'http://localhost:4173';
+      const redirect_uri = 'https://maximebarber.github.io/dust-shtats/';
       const scopes = 'user-read-private user-read-email';
       const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=code`;
 
@@ -29,7 +30,8 @@ export default {
     async getToken(code) {
       const client_id = 'd5c90c3559e747e598fe18eccc6cd1a0'; // Your client id
       const client_secret = '643bf177ae114d369d9d3a7eab2fa1fb'; // Your secret
-      const redirect_uri = 'http://localhost:4173'; // Your redirect uri
+      //const redirect_uri = 'http://localhost:4173'; // Your redirect uri
+      const redirect_uri = 'https://maximebarber.github.io/dust-shtats/'; // Your redirect uri
 
       const data = {
         grant_type: 'authorization_code',
